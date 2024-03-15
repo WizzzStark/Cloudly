30a1387e568d3049c949ee8ee5401f0e639c54d8
WizzzStark <alberto.fernandezv@udc.es>
2024-03-15 23:49:17 +0100

    Changed UI when no files uploaded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { api } from "../../convex/_generated/api";
 
 import UploadButton from "./uploadButton";
 import { FileCard } from "./file-card";
+import Image from "next/image";
 
 export default function Home() {
   const organization = useOrganization();
@@ -24,12 +25,31 @@ export default function Home() {
   return (
     <main className="container mx-auto pt-12">
 
-      <div className="flex justify-between mb-8">
-        <h1 className="text-4xl font-bold">Your Files</h1>
-        <UploadButton />
-     
-      </div>
+      {files && files?.length === 0 && (
+        <div className="flex flex-col gap-8 w-full items-center mt-24">
+            <Image 
+            alt="an image of files in a folder"
+            width="400"
+            height="400"
+            src="/empty.svg"
+            />
+            <div className="text-2xl">
+              You dont have any files yet. Upload some to get started.
+            </div>
+            <UploadButton />
+        </div> 
+      )}
 
+      {
+        files && files?.length > 0 && (
+         <>
+          <div className="flex justify-between mb-8">
+            <h1 className="text-4xl font-bold">Your Files</h1>
+            <UploadButton />
+          </div>
+         </>
+        )
+      }
       <div className="grid grid-cols-4 gap-4">
         {files?.map((file) => {
             return <FileCard key={file._id} file={file}/>
